c6388d37dddac4d97079e2ff36927c0772bd7833
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-02-04 17:56:50 -0300

    fix Guardar credenciais do bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,25 @@
+import * as dotenv from 'dotenv';
+
 import { AuthModule } from './application/auth/auth.module';
 import { ClassEntity } from './infrastructure/persistence/entities/class_entity';
 import { ClassModule } from './application/class/class.module';
+import { ConfigModule } from '@nestjs/config';
 import { LessonPlanEntity } from './infrastructure/persistence/entities/lesson_plan_entity';
 import { LessonPlanModule } from './application/lesson_plan/lesson_plan.module';
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './infrastructure/persistence/entities/user_entity';
 import { UserModule } from './application/user/user.module';
 
+dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      database: 'neondb',
-      host: 'ep-late-truth-a4mt8vu9-pooler.us-east-1.aws.neon.tech',
-      username: 'neondb_owner',
-      password: 'npg_iOmurBf3UyL0',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       port: 5432,
       entities: [UserEntity, ClassEntity, LessonPlanEntity],
       synchronize: true,
